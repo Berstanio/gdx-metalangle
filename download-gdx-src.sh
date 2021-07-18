67fbbe4918a789e474a3cac4812d2cf059ce0400
berstanio <sebastian_grunwald@web.de>
2021-07-18 21:48:34 +0200

    Build also robovm and moe backends.

--- download-gdx-src.sh
+++ download-gdx-src.sh
@@ -15,9 +15,20 @@ mkdir -p $BUILD_DIR
 cd $BUILD_DIR
 
 #The root files still get fetched
-git clone --depth 1 --filter=blob:none --sparse https://github.com/libgdx/libgdx
+git clone --depth 1 --filter=blob:none --sparse https://github.com/Berstanio/libgdx -b metalkit-bindings
 cd libgdx
 git sparse-checkout set gdx/src/
+git sparse-checkout add backends/gdx-backend-robovm/
+git sparse-checkout add backends/gdx-backend-moe/
+git sparse-checkout add pom.xml
+
+cd backends/gdx-backend-robovm
+sed -i 's/gdx-backend-robovm/gdx-backend-robovm-metalangle/g' pom.xml
+mvn install
+cd ..
+cd gdx-backend-moe
+sed -i 's/gdx-backend-moe/gdx-backend-moe-metalangle/g' pom.xml
+mvn install
 
 cd $JAVADIR
 #Make symbolic link in the src folder so jnigen can find them
