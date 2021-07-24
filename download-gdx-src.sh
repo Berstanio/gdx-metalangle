a2ee8e59585f940bd29544199b7c44141898c4e2
berstanio <sebastian_grunwald@web.de>
2021-07-24 14:40:07 +0200

    Configure poms and scripts to deploy to osshr.

--- download-gdx-src.sh
+++ download-gdx-src.sh
@@ -22,18 +22,20 @@ git sparse-checkout add backends/gdx-backend-robovm/
 git sparse-checkout add backends/gdx-backend-moe/
 git sparse-checkout add pom.xml
 
+/bin/cp $BASE/poms/pom-base.xml pom.xml
+
 cd backends/gdx-backend-robovm
 # Maybe this can be reduced to a singe sed?
 sed -i '' 's/gdx-backend-robovm/gdx-backend-robovm-metalangle/g' pom.xml
 sed -i '' 's/project.groupId/parent.groupId/g' pom.xml
-sed -i '' '/^.*gdx-backend-robovm-metalangle.*/i \ \ <groupId>io.github.berstanio</groupId>' pom.xml
-mvn install
+awk '/gdx-backend-robovm-metalangle/{print "  <groupId>io.github.berstanio</groupId>"}1' pom.xml > pom.tmp.xml && mv pom.tmp.xml pom.xml
+mvn deploy
 cd ..
 cd gdx-backend-moe
 sed -i '' 's/gdx-backend-moe/gdx-backend-moe-metalangle/g' pom.xml
 sed -i '' 's/project.groupId/parent.groupId/g' pom.xml
-sed -i '' '/.*gdx-backend-moe-metalangle.*/i \ \ <groupId>io.github.berstanio</groupId>' pom.xml
-mvn install
+awk '/gdx-backend-moe-metalangle/{print "  <groupId>io.github.berstanio</groupId>"}1' pom.xml > pom.tmp.xml && mv pom.tmp.xml pom.xml
+mvn deploy
 
 cd $JAVADIR
 #Make symbolic link in the src folder so jnigen can find them
